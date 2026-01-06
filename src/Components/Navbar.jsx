bb4d3ad223709f40216434e127a63779257a6937
Akash-715 <akaskrs7190@gmail.com>
2026-01-06 10:44:42 +0530

    Pushed files with home page completion

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+
+  const [scrolled , setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    }
+
+    window.addEventListener("scroll" , handleScroll);
+
+    return () => window.removeEventListener("scroll" , handleScroll)
+
+  } , [])
+
+
+  return (
+    <div>
+      <div className = {`mainNav ${scrolled ? "scrolled" : ""}`} >
+
+        <ul>
+            <li id = "NavCompanyTag">Company Name & Logo</li>
+            <li><NavLink to = "/">Home</NavLink></li>
+            <li><NavLink to = "/about">About</NavLink></li>
+            <li><NavLink to = "/courses">Courses</NavLink></li>
+            <li><NavLink to = "/contact">Contact us </NavLink></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
