340a99e761853a7def6e8a704cf9ebe4e1e7c502
Akash-715 <akashrs7190@gmail.com>
2026-01-11 01:10:24 +0530

    Course content div is added in course page.

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './course.css';
 import Footer from '../Components/Footer';
 
 const Courses = () => {
+
+    const [activeIndex , setActiveIndex] = useState(null);
+
+    const courseContent = [
+      {
+        Title: "Semiconductor Overview" ,
+        Tags: {Duration: ""} ,
+        Modules : ["ASIC/FPGA Technologies & Flow" , "EDA Tools"]
+      } , 
+      {
+        Title: "Digital Electronics",
+        Tags: {Duration: ""},
+        Modules: ["Introduction To Logic Gates" , "Combinational/Sequential Logic Circuits" ,
+                  "Frequency Division Using Counters" , "Finite State Machine" ,
+                  "Synchronous and Asynchronous Digital Circuits" , "Clock Domain Crossing, Low Power Design Techniques",
+                  "Setup And Hold Time"]
+      },
+      {
+        Title: "Verilog HDL" ,
+        Tags: {Duration: ""} ,
+        Modules: ["Introduction to Verilog, Data Types, Operators" , "Compiler Directives And System Tasks" , 
+                  "Task & Function, Data Flow Modeling" , "Behavioral Modeling, Verilog For Design",
+                  "Combinational/Sequential Logic Design" , "Synthesizable RTL For Design" ,
+                  "Verilog For Verification, Timing Controls" , "Conditional Statements, Loops",
+                  "Sequential And Parallel Blocks" , "Advanced Verilog For Verification, Timing Checks"
+        ]
+      },
+      {
+        Title: "Verification Concepts",
+        Tags: {Duration: ""} ,
+        Modules: ["Real-time verification scenarios"]
+      },
+      {
+        Title: "System Verilog",
+        Tags: {Duration: ""} ,
+        Modules: ["Data Types – Arrays, Queue" , "Array Manipulation Methods" ,
+                  "Structures, Unions, Task And Function" , "Task and Functions" ,
+                  "Interface, Virtual Interface, SV Classes" , "Mailbox, Packages, Compilation Unit, Processes",
+                  "Randomization And Constraints, SV Assertions"
+        ]
+      },
+      {
+        Title: "Protocol Training" ,
+        Tags: {Duration: ""} ,
+        Modules: ["PCIe or DDR or AXI"]
+      },
+      {
+        Title: "UVM" ,
+        Tags: {Duration: ""} ,
+        Modules: ["UVM Introduction, Factory and Overrides" , "UVM Reporting, Transaction Level Modeling",
+                  "UVM Driver, Sequencer, Monitor, Agent" , "UVM Environment, Test, Phases, Sequence",
+                  "Virtual Sequence and Virtual Sequencer" , "UVM Scoreboard, Callbacks, Configuration Management",
+                  "Register Abstraction Layer(RAL)" , "Functional Coverage Model and Subscriber",
+                  "Reference Model"
+        ]
+      },
+      {
+        Title: "Real Time Project Training",
+        Tags: {Duration: ""} ,
+        Modules: ["Students will get a chance to work on a Active Real Time Project involving AXI Interconnect/PCI Express and build their professional expertise."]
+      }
+    ]
+
+
+    
   return (
     <div className='course'>
       <div className='courseHero'>
@@ -30,6 +95,7 @@ const Courses = () => {
           <h2>What Makes This Course Stand Out</h2>
 
           <ul>
+
             <li>Strong foundation in Digital Design and Verilog</li>
             <li>Coverage of state-of-the-art VLSI verification concepts</li>
             <li>Extensive training in SystemVerilog and UVM</li>
@@ -63,11 +129,37 @@ const Courses = () => {
           </ul>
 
         </div>
+      </div>
 
         <div className='courseContent'>
-          
+          <h1>Course Content</h1>
+
+              {courseContent.map((course, index) => (
+                <div key={index} className="courseBlock">
+
+
+                  <div 
+                    className="courseHeader"
+                    onClick={() => setActiveIndex(activeIndex === index ? null : index)}
+                  >
+                    <span className="courseTitle">{course.Title}</span>
+                    <span className="more">
+                      {activeIndex === index ? "Hide Details ↗" : "More Details ↘"}
+                    </span>
+                  </div>
+
+
+                  <div className={`modules ${activeIndex === index ? "active" : ""}`}>
+                    <ul>
+                      {course.Modules.map((module, i) => (
+                        <li key={i}>{module}</li>
+                      ))}
+                    </ul>
+                  </div>
+
+                </div>
+              ))}
         </div>
-      </div>
 
 
     <Footer />
